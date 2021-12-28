aeca1fba41d2fcff68b9f3ed5df4a863531a4b3f
SafaTormane <94962496+SafaTormane@users.noreply.github.com>
2021-12-28 15:29:25 +0100

    Add files via upload

---
+++ chekpoint DOM/script.js
@@ -0,0 +1,13 @@
+var btnAjouter = document.querySelector(".btn-Ajouter");
+var btnCoeur = document.querySelector(".btn-Coeur");
+var container = document.querySelector(".container");
+console.log(container);
+
+btnAjouter.addEventListener("click", () => {
+  var row = document.createElement("div");
+
+  row.className = "row";
+  var col = document.addEventListener("div");
+  col.className = "col";
+  console.log(text);
+});
